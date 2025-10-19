ecd25e8656a3342731a00af2792cb35231a70d83
Vannyou Tang <vannyoutang23@gmail.com>
2025-10-19 18:06:33 +0700

    chore: :rocket: add json formatter

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,32 @@ import {
   TrashIcon,
 } from "lucide-react";
 import { useState } from "react";
+import { useTimeout } from "usehooks-ts";
 
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { ModeToggle } from "@/components/ModeToggle";
 
 const App = () => {
   const [json, setJson] = useState("");
+
+  const [isCopied, setIsCopied] = useState(false);
+  const [isPasted, setIsPasted] = useState(false);
+
+  useTimeout(
+    () => {
+      setIsCopied(false);
+    },
+    isCopied ? 2000 : null
+  );
+
+  useTimeout(
+    () => {
+      setIsPasted(false);
+    },
+    isPasted ? 2000 : null
+  );
 
   const handleFormatJson = () => {
     if (!json.trim()) {
@@ -48,77 +67,82 @@ const App = () => {
     setJson("");
   };
 
-  const handleCopy = () => {
+  const handleCopy = async () => {
     if (!json.trim()) {
       alert("JSON is empty");
       return;
     }
 
-    navigator.clipboard.writeText(json);
+    await navigator.clipboard.writeText(json);
+    setIsCopied(true);
   };
 
   const handlePaste = async () => {
     const text = await navigator.clipboard.readText();
     setJson(text);
+    setIsPasted(true);
   };
 
   return (
-    <main className="h-screen bg-gray-300 flex flex-col p-2 gap-2">
-      <div className="flex gap-4">
-        <div className="flex gap-2">
+    <main className="h-screen bg-border grid grid-rows-[min-content_calc(100vh-3.5rem)] p-2 gap-2">
+      <div className="flex justify-between h-8">
+        <div className="flex gap-4">
+          <div className="flex gap-2">
+            <Button
+              size="sm"
+              className="active:scale-95 transition-transform cursor-pointer min-w-50"
+              onClick={handleFormatJson}
+            >
+              <BracesIcon />
+              &nbsp;Format JSON
+            </Button>
+            <Button
+              size="sm"
+              className="active:scale-95 transition-transform cursor-pointer min-w-50"
+              onClick={handleRemoveWhitespaces}
+            >
+              <BrushCleaningIcon />
+              &nbsp;Remove Whitespaces
+            </Button>
+          </div>
+          <Separator orientation="vertical" className="bg-muted-foreground" />
+          <div className="flex gap-2">
+            <Button
+              size="sm"
+              className="active:scale-95 transition-transform cursor-pointer min-w-25"
+              onClick={handleCopy}
+            >
+              <ClipboardCopyIcon />
+              &nbsp;{isCopied ? "Copied" : "Copy"}
+            </Button>
+            <Button
+              size="sm"
+              className="active:scale-95 transition-transform cursor-pointer min-w-25"
+              onClick={handlePaste}
+            >
+              <ClipboardPasteIcon />
+              &nbsp;{isPasted ? "Pasted" : "Paste"}
+            </Button>
+          </div>
+          <Separator orientation="vertical" className="bg-muted-foreground" />
           <Button
             size="sm"
-            className="active:scale-95 transition-transform cursor-pointer min-w-50"
-            onClick={handleFormatJson}
+            variant="outline"
+            className="active:scale-95 transition-transform cursor-pointer"
+            onClick={handleClear}
           >
-            <BracesIcon />
-            &nbsp;Format JSON
-          </Button>
-          <Button
-            size="sm"
-            className="active:scale-95 transition-transform cursor-pointer min-w-50"
-            onClick={handleRemoveWhitespaces}
-          >
-            <BrushCleaningIcon />
-            &nbsp;Remove Whitespaces
+            <TrashIcon />
+            &nbsp;Clear
           </Button>
         </div>
-        <Separator orientation="vertical" className="bg-gray-800" />
-        <div className="flex gap-2">
-          <Button
-            size="sm"
-            className="active:scale-95 transition-transform cursor-pointer"
-            onClick={handleCopy}
-          >
-            <ClipboardCopyIcon />
-            &nbsp;Copy
-          </Button>
-          <Button
-            size="sm"
-            className="active:scale-95 transition-transform cursor-pointer"
-            onClick={handlePaste}
-          >
-            <ClipboardPasteIcon />
-            &nbsp;Paste
-          </Button>
-        </div>
-        <Separator orientation="vertical" className="bg-gray-800" />
-        <Button
-          size="sm"
-          variant="secondary"
-          className="active:scale-95 transition-transform cursor-pointer"
-          onClick={handleClear}
-        >
-          <TrashIcon />
-          &nbsp;Clear
-        </Button>
+        <ModeToggle />
       </div>
       <div className="flex-1 flex">
         <Textarea
           spellCheck="false"
           autoComplete="off"
           autoCapitalize="off"
-          className="bg-gray-200 flex-1 resize-none font-mono border-2 border-solid border-gray-400 focus-visible:ring-0"
+          className="bg-muted flex-1 resize-none font-mono border-2 border-solid border-muted-foreground focus-visible:ring-0 focus:ring-0"
           value={json}
           onChange={(e) => {
             setJson(e.target.value);
