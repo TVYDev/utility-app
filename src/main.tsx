7fdfa586c5bd7a45de6968278296edd9e42d088f
Vannyou Tang <vannyoutang23@gmail.com>
2025-10-20 23:47:33 +0700

    chore: :rocket: add web analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/ThemeProvider.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="utility-app-ui-theme">
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>
 );
