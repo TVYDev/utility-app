8618796dae8ed710b59d145222e266285d1de473
Vannyou Tang <vannyoutang23@gmail.com>
2025-10-25 17:57:05 +0700

    chore: :rocket: add offline support for other route

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -7,5 +7,22 @@ module.exports = {
   skipWaiting: true,
   maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
   cleanupOutdatedCaches: true,
-  cacheId: "toolifybox-v1.0.10",
+  cacheId: "toolifybox-v1.0.11",
+  // Handle navigation requests (SPA routing)
+  navigateFallback: "/index.html",
+  navigateFallbackDenylist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
+  // Add runtime caching for navigation requests
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/www\.toolifybox\.app\//,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "pages",
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 7, // 1 week
+        },
+      },
+    },
+  ],
 };
